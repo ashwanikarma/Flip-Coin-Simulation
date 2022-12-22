154aaaa2dd9c88e583adaa73d87520c707d41b89
ashwanikarma <ashwanikarma9@gmail.com>
2022-12-22 19:39:17 +0530

    Added Loop through multiple times UC2

--- flipCoinSimulator.sh
+++ flipCoinSimulator.sh
@@ -1,8 +1,10 @@
-coin=$((RANDOM%2));
-
-if [ $coin -eq 0 ]
-then 
-	echo "Head";
-else
-	echo "Tail";
-fi
+for(( i=0; i<=10; i++ ))
+do
+	coin=$((RANDOM%2));
+	if [ $coin -eq 0 ]
+	then
+ 		echo "Head";
+	else
+		echo "Tail";
+	fi
+done
