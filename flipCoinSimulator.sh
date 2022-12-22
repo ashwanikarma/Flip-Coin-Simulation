919c797f0469446a923c11e5dfd77617a4b81bfc
ashwanikarma <ashwanikarma9@gmail.com>
2022-12-22 21:31:00 +0530

    Added UC-4

--- flipCoinSimulator.sh
+++ flipCoinSimulator.sh
@@ -2,6 +2,7 @@ HeadWin=0;
 TailWin=0;
 while [[ $HeadWin -lt 21 && $TailWin -lt 21 ]]
 do
+
 	coin=$((RANDOM%2));
 
 	if [ $coin -eq 0 ]
@@ -13,22 +14,34 @@ do
 		echo "Tail";
 	fi
 	
-	if [ $HeadWin -eq $TailWin ]
-	then
-		HeadWin=0;
-		TailWin=0;
-	fi
    	
 done
-
+HeadWin=10;
+TailWin=10;
 if [ $HeadWin -gt $TailWin ]
 then
-	echo "Head won by $HeadWin Point";	
-	echo "Tail lose by $TailWin Point";
+	echo "Head won by $(($HeadWin-$TailWin)) Point";	
 elif [ $TailWin -gt $HeadWin ]
 then	
-	echo "Tail won by $TailWin Point";
-	echo "Head lose by $HeadWin Point";
+	echo "Tail won by $(($TailWin-$HeadWin)) Point";
 else
 	echo "Tie";
+	HeadWin=0;
+	TailWin=0;
+	while [[ $(($HeadWin-$TailWin)) -lt 2 ]]
+	do
+
+	coin=$((RANDOM%2));
+
+	if [ $coin -eq 0 ]
+	then
+		HeadWin=$(($HeadWin+1));
+ 		echo "Head";
+	else
+		TailWin=$(($TailWin+1));
+		echo "Tail";
+	fi
+	done
+	echo "Head= $HeadWin";
+	echo "Tail= $TailWin";
 fi
