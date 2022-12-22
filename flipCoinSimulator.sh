f61a9b61a447e01b197a34233ba862a3c51dc88c
ashwanikarma <ashwanikarma9@gmail.com>
2022-12-22 19:48:23 +0530

    Updated file which shows who won by greater points

--- flipCoinSimulator.sh
+++ flipCoinSimulator.sh
@@ -1,10 +1,24 @@
+HeadWin=0;
+TailWin=0;
 for(( i=0; i<=10; i++ ))
 do
 	coin=$((RANDOM%2));
 	if [ $coin -eq 0 ]
 	then
+		HeadWin=$(($HeadWin+1));
  		echo "Head";
 	else
+		TailWin=$(($TailWin+1));
 		echo "Tail";
 	fi
 done
+
+if [ $HeadWin -gt $TailWin ]
+then
+	echo "Head won by $HeadWin Point";
+elif [ $TailWin -gt $HeadWin ]
+then	
+	echo "Tail won by $TailWin Point";
+else
+	echo "Tie";
+fi
